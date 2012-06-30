6e1f8b9eabb9ce5dac2756364ebbb3bbe4ef1b15
Keith Gaughan <k@stereochro.me>
2012-06-30 23:53:56 +0100

    More views; templates.

--- thirdplace/app.py
+++ thirdplace/app.py
@@ -1,6 +1,28 @@
+import flask
+
 from thirdplace.core import app
 
 
 @app.route('/')
-def hello():
-    return "Hello, world!"
+def show_forums():
+    return flask.render_template('forums.html')
+
+
+@app.route('/<int:forum_id>/')
+def show_forum(forum_id):
+    return ""
+
+
+@app.route('/<int:forum_id>/<int:topic_id>/')
+def show_topic(forum_id, topic_id):
+    return ""
+
+
+@app.route('/users/<int:user_id>/')
+def show_user(user_id):
+    return ""
+
+
+@app.route('/users/<int:user_id>/posts/')
+def show_user_posts(user_id):
+    return ""
