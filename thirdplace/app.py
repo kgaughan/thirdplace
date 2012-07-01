a21effb88061fcf26c367dcc5e5dd9185a6dda84
Keith Gaughan <k@stereochro.me>
2012-07-02 00:25:49 +0100

    Views for topics and posts.

--- thirdplace/app.py
+++ thirdplace/app.py
@@ -12,12 +12,21 @@ def show_forums():
 
 @app.route('/<int:forum_id>/')
 def show_topics(forum_id):
-    return ""
+    forum = models.Forum.query.get_or_404(forum_id).forum
+    topics = models.Topic.query_for_forum(forum_id)
+    return flask.render_template('topics.html', topics=topics, forum=forum)
 
 
 @app.route('/<int:forum_id>/<int:topic_id>/')
 def show_posts(forum_id, topic_id):
-    return ""
+    topic = models.Topic.query.get_or_404(topic_id)
+    if topic.forum_id != forum_id:
+        flask.abort(404)
+    posts = models.Post.query_for_topic(topic_id)
+    return flask.render_template(
+        'posts.html',
+        forum_id=forum_id, forum=topic.forum.forum, topic=topic.topic,
+        posts=posts)
 
 
 @app.route('/users/<int:user_id>/')
